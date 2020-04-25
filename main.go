9f89428fd71438eb0455365894736018318e6dbd
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-25 15:17:32 -0600

    Add files to kaizen api to bring in github data for rails

--- main.go
+++ main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	user := User{Firstname: "Steve"}
-	fmt.Println(user)
+	fmt.Println("Starting Service...")
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	NewRouter(router)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+	fmt.Println("Service Terminated...")
 }
---
+++ router.go
@@ -0,0 +1,9 @@
+package main
+
+import(
+	"github.com/gorilla/mux"
+)
+
+func NewRouter(router *mux.Router){
+	router.Methods("GET").Path("/github_user").HandlerFunc(FetchGHUser)
+}
