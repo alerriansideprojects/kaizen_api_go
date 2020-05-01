bf0cf7ff1b816e81ffa9cb38492ce3d9b38ae324
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-05-01 17:39:41 -0600

    Add heroku port thing

--- main.go
+++ main.go
@@ -15,6 +15,21 @@ func main() {
 
 	NewRouter(router)
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	err := http.ListenAndServe(GetPort(), router)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
 	fmt.Println("Service Terminated...")
+}
+
+func GetPort() string {
+	var port = os.Getenv("PORT")
+
+	if port == "" {
+		port = "4747"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+
+	return ":" + port
 }
