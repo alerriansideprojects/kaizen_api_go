016792bad963084129fcdffc237d0fea5aacb35c
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-05-01 18:19:44 -0600

    Fix styling

--- router.go
+++ router.go
@@ -1,10 +1,10 @@
 package main
 
-import(
+import (
 	"github.com/gorilla/mux"
 )
 
-func NewRouter(router *mux.Router){
+func NewRouter(router *mux.Router) {
 	router.Methods("GET").Path("/gh_user").HandlerFunc(FetchGHUser)
 	router.Methods("GET").Path("/so_badges").HandlerFunc(FetchSOUser)
 	router.Methods("GET").Path("/").HandlerFunc(FetchIndex)
